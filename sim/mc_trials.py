7be7072b2d601451c3dce011c93e73435ef0347d
Kyle MacNeney <eazymac25@yahoo.com>
2018-03-18 13:35:21 -0400

    updating structure of states

--- sim/mc_trials.py
+++ sim/mc_trials.py
@@ -63,46 +63,29 @@ class State(object):
 				return self.SUCCESS
 			return self.FAIL
 
-class PreTrial(State):
-	
-	def transition(self, p):
+	def transition(self, p, success_state):
 		if self.val == self.NOT_STARTED:
 			trial_outcome = self.attempt_trial(p)
 			self.val = trial_outcome
 			if trial_outcome == self.SUCCESS:
-				return P1()
+				return success_state()
+			return Fail()
 		return self
 
+class PreTrial(State):
+	pass
 
 class P1(State):
-
-	def transition(self, p):
-		if self.val == self.NOT_STARTED:
-			trial_outcome = self.attempt_trial(p)
-			self.val = trial_outcome
-			if trial_outcome == self.SUCCESS:
-				return P2()
-		return self
+	pass
 
 class P2(State):
-
-	def transition(self, p):
-		if self.val == self.NOT_STARTED:
-			trial_outcome = self.attempt_trial(p)
-			self.val = trial_outcome
-			if trial_outcome == self.SUCCESS:
-				return P3()
-		return self
+	pass
 
 class P3(State):
+	pass
 
-	def transition(self, p):
-		if self.val == self.NOT_STARTED:
-			trial_outcome = self.attempt_trial(p)
-			self.val = trial_outcome
-			if trial_outcome == self.SUCCESS:
-				return Complete()
-		return self
+class NDA(State):
+	pass
 
 class Complete(State):
 
@@ -110,59 +93,64 @@ class Complete(State):
 		super(Complete, self).__init__()
 		self.val = self.SUCCESS
 
+class Fail(State):
+
+	def __init__(self):
+		super(Complete, self).__init__()
+		self.val = self.FAIL
+
 class ClinicalStages(object):
 
-	def __init__(self, start):
-		self.state = start
+	def __init__(self, stages=[]):
+		FST_STAGE = 0
+		self.state = stages[FST_STAGE]
+		self.stages = stages
 
 	def trial_runs(self, probs):
 		idx = 0
 		while True:
-			if self.state.val == State.FAIL:
+			if isinstance(self.state, Fail):
 				break
 			if isinstance(self.state, Complete):
 				yield self.state
 				break
-			new_state = self.state.transition(probs[idx])
+			new_state = self.state.transition(probs[idx], self.stages[idx])
 			yield self.state
 			self.state = new_state
 			idx += 1
 
 class Simulation(object):
 
-	def __init__(self, drug_id, num_trials, npv=None, start_stage=PreTrial, probs=[], distributions=[]):
+	state_map = {
+		'PRE': PreTrial
+		'P1': P1,
+		'P2': P2,
+		'P3': P3,
+		'NDA': NDA,
+		'DONE': Complete
+	}
+
+	def __init__(self, drug_id, num_trials, npv=None,
+		stage_list=('PRE','P1','P2','P3','NDA','DONE'), probs=[], distributions=[]):
 
 		self.drug_id = drug_id
 		self.num_trials = num_trials
 		self.npv = npv
-		self.start_stage = start_stage
+		self.stage_list = stage_list
 		self.probs = probs
 		self.distributions = distributions
 
 	def run_simulation(self):
-		stop_point_counts = [0]*self.get_stop_point_len()
+		stages = self.get_stages()
+		stop_point_counts = [0]*len(stages)
 		for run in range(self.num_trials):
-			new_start = self.start_stage()
-			stages = ClinicalStages(new_start)
+			stages = ClinicalStages(stages=stages)
 			outcomes =[trial.val for trial in stages.trial_runs(self.probs)]
 			stop_point_counts[len(outcomes)-1] += 1
 		return stop_point_counts
 
-
-	def get_stop_point_len(self):
-		"""
-		I really can't think of a good way to do this
-		"""
-		if self.start_stage == PreTrial:
-			return 5
-		if self.start_stage == P1:
-			return 4
-		if self.start_stage == P2:
-			return 3
-		if self.start_stage == P3:
-			return 2
-		if self.start_stage == Complete:
-			return 1
+	def get_stages(self):
+		return [self.state_map.get(stage) for stage in self.stage_list]
 
 if __name__ == '__main__':
 	sim = Simulation(1234, 100000, probs=[.7, .3, .5, .40])
